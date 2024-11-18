666ff765c97b3dedcbdae76532c3ac4aacf5627d
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-11-18 16:22:45 +0100

    first commit

---
+++ hobbies.js
@@ -0,0 +1,10 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+  this.firstHobby = hobbyOne;
+  this.secondHobby = hobbyTwo;
+  this.thirdHobby = hobbyThree;
+}
+
+// const testObj = new hobbies("reading", "drawing", "cooking");
+// console.log(testObj);
+
+module.exports = hobbies;
